23397cd71fc72f2186aff81f770b06c4240986e2
Max Kipka <max.kipka@icloud.com>
2026-03-06 21:44:16 +0100

    changed background position to the background

--- models/world.class.js
+++ models/world.class.js
@@ -23,10 +23,11 @@ export class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.backgroundObjects);
+        
 
         // ? Draw wird immer wieder neu aufgerufen
         let self = this;
