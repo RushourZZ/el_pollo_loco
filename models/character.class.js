4ee9e9e608cde4dd53c0fae1a6f888ae39411896
Max Kipka <max.kipka@icloud.com>
2026-03-06 22:53:54 +0100

    scaled objects(character + chicken)

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,8 @@ import { MovableObject } from "./movable-object.class.js";
 import { ImageHub } from "../manager_classes/imageHub.js";
 
 export class Character extends MovableObject {
+height = 280;
+y = 155;
     constructor() {
         super().loadImage(
             ImageHub.CHARACTER.idle[0],
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,11 +2,11 @@ import { MovableObject } from "./movable-object.class.js";
 import { ImageHub } from "../manager_classes/imageHub.js";
 
 export class Chicken extends MovableObject {
-    y = 290
+    y = 370;
+    height = 70;
+    width = 70;
     constructor() {
-        super().loadImage(
-            ImageHub.ENEMIES_CHICKEN_NORMAL.walk[0],
-        );
+        super().loadImage(ImageHub.ENEMIES_CHICKEN_NORMAL.walk[0]);
 
         this.x = 200 + Math.random() * 500;
     }
